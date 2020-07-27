6e2e6195822b401daa0b26a88d95791640f8debc
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-07-27 09:15:42 -0500

    adding new flags

--- cmake/EnableBoostSupport.cmake
+++ cmake/EnableBoostSupport.cmake
@@ -20,8 +20,9 @@ MACRO(ENABLE_BOOST_SUPPORT)
     # component from being appended to the list. Unit tests that have been
     # written using the boost unit test API need to add ${BOOST_TEST_LIBRARY}
     # to the TARGET_LINK_LIBRARIES macro call.
-    FIND_PACKAGE(Boost 1.72.0 REQUIRED COMPONENTS test_exec_monitor)
+    FIND_PACKAGE(Boost 1.72.0 REQUIRED COMPONENTS program_options test_exec_monitor)
 
+    #add_definitions(-D_GLIBCXX_USE_CXX11_ABI=1)
     IF(${CMAKE_BUILD_TYPE} EQUAL RELEASE)
       SET(BOOST_TEST_LIBRARY ${Boost_TEST_EXEC_MONITOR_LIBRARY_RELEASE})
     ELSE()
